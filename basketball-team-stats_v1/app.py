8a63c42f6e90cd7825dcf46469562b971baefd9e
Aaron Lenhart <aaron.lenhart1@gmail.com>
2019-04-28 09:01:02 -0400

    adding height and changing values

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -1,5 +1,4 @@
-import constants
-
+import constants 
 
 THE_TEAMS = constants.TEAMS
 the_list_of_players = constants.PLAYERS
@@ -8,21 +7,28 @@ non_experienced_players = []
 PANTHERS = []
 BANDITS = []
 WARRIORS = []
-
+avg_height_exp = []
+avg_height_inexp = []
 
 def sort_players():
     global PANTHERS, BANDITS, WARRIORS
     for player in the_list_of_players:
-        if (player['experience']=='YES'):
+        if (player['experience']== 'YES'):
+            player['experience'] = True
+            player['height'] = int(player["height"][:2])
             experienced_players.append(player['name'])
+            avg_height_exp.append(player['height'])
         else:
-            non_experienced_players.append(player['name']) 
+            non_experienced_players.append(player['name'])
+            player['experience'] = True
+            player['height'] = int(player["height"][:2])
+            avg_height_inexp.append(player['height'])
     PANTHERS = experienced_players[:3] + non_experienced_players[:3]
     BANDITS = experienced_players[3:6] + non_experienced_players[3:6]
     WARRIORS = experienced_players[6:] + non_experienced_players[6:]
     return(PANTHERS, BANDITS, WARRIORS)
- 
-    
+
+
 def print_tools_name():
     print("BASKETBALL TEAM STATS TOOL \n \n" + "-"*15 + "Menu" + "-"*15)
 
@@ -43,15 +49,19 @@ def show_players_on_team():
                         print(player, end=' ')
                     else:
                         print(player+",", end=' ')
+                avg = (sum(avg_height_exp[:3]) + sum(avg_height_inexp[:3]))/6
+                print("\nAverage height of team:", round(avg) , "inches")
                 break  
             elif chosen_pick == 2:          
                 print("\n Team: Bandits Stats \n" + "-"*25  + "\n Total players: {} \n".format(len(BANDITS)))
                 print("Players on Team:")
                 for player in BANDITS:
                     if player == BANDITS[-1]:
-                    	print(player, end=' ')
+                    	   print(player, end=' ')
                     else:
                         print(player+",", end=' ')
+                avg = (sum(avg_height_exp[3:6]) + sum(avg_height_inexp[3:6]))/6
+                print("\nAverage height of team:", round(avg) , "inches")        
                 break 
             elif chosen_pick == 3:          
                 print("\n Team: Warriors Stats \n" + "-"*25 + "\n Total players: {}\n".format(len(WARRIORS)))
@@ -61,11 +71,13 @@ def show_players_on_team():
                         print(player, end=' ')
                     else:
                         print(player+",", end=' ')
+                avg = (sum(avg_height_exp[3:6]) + sum(avg_height_inexp[3:6]))/6
+                print("\nAverage height of team:", round(avg) , "inches")          
                 break
         except ValueError:
             print("Sorry about that we where expect a 1 for Panthers and a 2 for Bandits or 3 for Warriors try again")
         else:
-	    	print("Sorry about that we where expect a 1 for Panthers and a 2 for Bandits or 3 for Warriors try again")
+	    	      print("Sorry about that we where expect a 1 for Panthers and a 2 for Bandits or 3 for Warriors try again")
 
 def run_menu():
     print(" \n Here are your choices: \n 1) {} \n 2) {} \n ".format("Display Team Stats","Quit"))
